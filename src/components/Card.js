bf6d0b3f931e59dc6d11ef23b8df8eedda8c863f
Alex Pavlov <alex.pavlov.948@gmail.com>
2022-10-04 02:34:54 +0600

    - добавил сборку react - перенеёс стили - реализовал компоненты попап, карточки, main, header, footer

---
+++ src/components/Card.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Card({card, onCardClick}) {
+    function handleClick() {
+        onCardClick(card);
+    }
+
+    return(
+        <article className="card">
+            <button className="card__image-button" onClick={handleClick}>
+                <img className="card__image" src="src/components/App#" alt="#" style={{ backgroundImage: `url(${card.link})` }} />
+            </button>
+            <div className="card__info">
+                <h3 className="card__text">{card.name}</h3>
+                <div className="card__button-wrapper">
+                    <button className="card__button"></button>
+                    <span className="card__count-like">{card.likes.length}</span>
+                </div>
+            </div>
+            <button className="card__trash"></button>
+        </article>
+    );
+}
+
+export default Card;
